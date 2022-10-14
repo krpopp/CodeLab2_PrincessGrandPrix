bc86fc9e5e906ad2c99abe5dd6ad4b602e731f74
Zhiyue Huang <zhiyuehuang@yeah.net>
2022-10-14 15:31:29 -0400

    Upload JZ_Heuristic

---
+++ Assets/Students/_StudentTemplate/Scripts/JZ_Heuristic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JZ_Heuristic : HueristicScript
+{
+    public override float Hueristic(int x, int y, Vector3 start, Vector3 goal, GridScript gridScript)
+    {
+        return 0;
+    }
+}
